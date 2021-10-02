bfd08db9038fa964646090f006b4fd9e4ca2b5c0
Zhengke <zhengke0509@foxmail.com>
2021-10-02 14:53:53 +0800

    this is NO.5

--- renderer.js
+++ renderer.js
@@ -16,8 +16,8 @@ const mount = (vnode, container) => {
     if (vnode.props) {
         for (const key in vnode.props) {
             const value = vnode.props[key];
-            if (key.startsWith('on')) {
-                el.addEventListener(key.slice(2).toLocaleLowerCase(), value);
+            if (key.startsWith('on')) { // 事件监听判断
+                el.addEventListener(key.slice(2).toLowerCase(), value);
             } else {
                 el.setAttribute(key, value);
             }
@@ -45,8 +45,68 @@ const patch = (n1, n2) => {
         const n1ElParent = n1.el.parentElement;
         n1ElParent.removeChild(n1.el); // 移除旧节点
         mount(n2, n1ElParent) // 挂载新节点
-        console.log('patch');
     } else {
+        // 1.取出element对象，并在n2中保存
         const el = n2.el = n1.el
+
+        // 2.处理props
+        const oldProps = n1.props || {};
+        const newProps = n2.props || {};
+
+        // 2.1 获取所有的newProps添加到el
+        for (const key in newProps) {
+            const oldValue = oldProps[key];
+            const newValue = newProps[key];
+            if (newValue !== oldValue) {
+                if (key.startsWith('on')) {
+                    el.addEventListener(key.slice(2).toLowerCase(), newValue);
+                } else {
+                    el.setAttribute(key, newValue)
+                }
+            }
+        }
+        // 2.2 删除旧的props
+        for (const key in oldProps) {
+            if (!(key in newProps)) {
+                if (key.startsWith('on')) {
+                    const value = oldProps[key]
+                    el.removeEventListener(key.slice(2).toLowerCase(), value)
+                } else {
+                    el.removeAttribute(key)
+                }
+            }
+        }
+
+        // 3.处理children
+        const oldChildren = n1.children || []
+        const newChildren = n2.children || []
+
+        if (typeof newChildren === 'string') { // 情况一：newChildren 本身是一个String
+            /* 边界判断（edge case）*/
+            if (typeof oldChildren === 'string') {
+                if (newChildren !== oldChildren) {
+                    el.textContent = newChildren
+                }
+            } else {
+                el.innerHTML = newChildren
+
+            }
+        } else { // 情况二：newChildren 本身是一个数组
+            if (typeof oldChildren === 'string') {
+                el.innerHTML = ''
+                newChildren.forEach(item => {
+                    mount(item, el)
+                })
+            } else {
+                /* 前面有相同节点的原生进行patch操作 */
+                // oldChildren [v1,v2,v3]
+                // newChildren [v1,v2,v3,v4,v5]
+                const commonLength = Math.min(oldChildren.length, newChildren.length) // 拿到更短的长度
+                for (let i = 0; i < commonLength; i++) {
+                    patch(oldChildren[i], newChildren[i])
+                }
+
+            }
+        }
     }
 }
